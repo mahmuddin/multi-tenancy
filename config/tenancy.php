21cdc5b5f94503d685911ae80a6283f144b98103
Mahmuddin Nurul Fajri <mahmuddinnf@gmail.com>
2025-06-19 05:14:48 +0700

    Refactor tenancy configuration for consistency and clarity

--- config/tenancy.php
+++ config/tenancy.php
@@ -6,19 +6,19 @@ use Stancl\Tenancy\Database\Models\Domain;
 use Stancl\Tenancy\Database\Models\Tenant;
 
 return [
-    'tenant_model'          => \App\Models\Tenant::class,
-    'id_generator'          => Stancl\Tenancy\UUIDGenerator::class,
+    'tenant_model'         => \App\Models\Tenant::class,
+    'id_generator'         => Stancl\Tenancy\UUIDGenerator::class,
 
     // 'domain_model'         => Domain::class,
     // Tidak menggunakan domain model untuk path-based
-    'domain_model'          => null,
+    'domain_model'         => null,
 
     /**
      * The list of domains hosting your central app.
      *
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
-    'central_domains'       => [
+    'central_domains'      => [
         '127.0.0.1',
         'localhost',
     ],
@@ -29,7 +29,7 @@ return [
      *
      * To configure their behavior, see the config keys below.
      */
-    'bootstrappers'         => [
+    'bootstrappers'        => [
         Stancl\Tenancy\Bootstrappers\DatabaseTenancyBootstrapper::class,
         Stancl\Tenancy\Bootstrappers\CacheTenancyBootstrapper::class,
         Stancl\Tenancy\Bootstrappers\FilesystemTenancyBootstrapper::class,
@@ -40,14 +40,14 @@ return [
     /**
      * Database tenancy config. Used by DatabaseTenancyBootstrapper.
      */
-    'database'              => [
+    'database'             => [
         'central_connection'         => env('DB_CONNECTION', 'central'),
 
         /**
          * Connection used as a "template" for the dynamically created tenant database connection.
          * Note: don't name your template connection tenant. That name is reserved by package.
          */
-        'template_tenant_connection' => 'tenant',
+        'template_tenant_connection' => null,
 
         /**
          * Tenant database names are created like this:
@@ -89,7 +89,7 @@ return [
      *
      * You can clear cache selectively by specifying the tag.
      */
-    'cache'                 => [
+    'cache'                => [
         'tag_base' => 'tenant', // This tag_base, followed by the tenant_id, will form a tag that will be applied on each cache call.
     ],
 
@@ -97,7 +97,7 @@ return [
      * Filesystem tenancy config. Used by FilesystemTenancyBootstrapper.
      * https://tenancyforlaravel.com/docs/v3/tenancy-bootstrappers/#filesystem-tenancy-boostrapper.
      */
-    'filesystem'            => [
+    'filesystem'           => [
         /**
          * Each disk listed in the 'disks' array will be suffixed by the suffix_base, followed by the tenant_id.
          */
@@ -149,7 +149,7 @@ return [
      * Redis tenancy is only relevant if you're making direct Redis calls,
      * either using the Redis facade or by injecting it as a dependency.
      */
-    'redis'                 => [
+    'redis'                => [
         'prefix_base'          => 'tenant', // Each key in Redis will be prepended by this prefix_base, followed by the tenant id.
         'prefixed_connections' => [         // Redis connections whose keys are prefixed, to separate one tenant's keys from another.
                                                 // 'default',
@@ -164,24 +164,13 @@ return [
      * See the documentation page for each class to
      * understand which ones you want to enable.
      */
-    'features'              => [
-        Stancl\Tenancy\Features\UserImpersonation::class,
-        Stancl\Tenancy\Features\TelescopeTags::class,
-                                                     // Stancl\Tenancy\Features\UniversalRoutes::class,
-        Stancl\Tenancy\Features\TenantConfig::class, // https://tenancyforlaravel.com/docs/v3/features/tenant-config
-                                                     // Stancl\Tenancy\Features\CrossDomainRedirect::class, // https://tenancyforlaravel.com/docs/v3/features/cross-domain-redirect
-                                                     // Stancl\Tenancy\Features\ViteBundler::class,
-    ],
-
-    // Storage path
-    'storage_to_config_map' => [
-        '%storage_path%/app/public'         => 'filesystems.disks.public.root',
-        '%storage_path%/app'                => 'filesystems.disks.local.root',
-        '%storage_path%/logs'               => 'logging.channels.single.path',
-        '%storage_path%/logs'               => 'logging.channels.daily.path',
-        '%storage_path%/framework/cache'    => 'cache.stores.file.path',
-        '%storage_path%/framework/sessions' => 'session.files',
-        '%storage_path%/framework/views'    => 'view.compiled',
+    'features'             => [
+        // Stancl\Tenancy\Features\UserImpersonation::class,
+        // Stancl\Tenancy\Features\TelescopeTags::class,
+        // Stancl\Tenancy\Features\UniversalRoutes::class,
+        // Stancl\Tenancy\Features\TenantConfig::class, // https://tenancyforlaravel.com/docs/v3/features/tenant-config
+        // Stancl\Tenancy\Features\CrossDomainRedirect::class, // https://tenancyforlaravel.com/docs/v3/features/cross-domain-redirect
+        // Stancl\Tenancy\Features\ViteBundler::class,
     ],
 
     /**
@@ -191,12 +180,12 @@ return [
      * enabled. But it may be useful to disable them if you use external
      * storage (e.g. S3 / Dropbox) or have a custom asset controller.
      */
-    'routes'                => true,
+    'routes'               => true,
 
     /**
      * Parameters used by the tenants:migrate command.
      */
-    'migration_parameters'  => [
+    'migration_parameters' => [
         '--force'    => true, // This needs to be true to run migrations in production.
         '--path'     => [database_path('migrations/tenant')],
         '--realpath' => true,
@@ -205,7 +194,7 @@ return [
     /**
      * Parameters used by the tenants:seed command.
      */
-    'seeder_parameters'     => [
+    'seeder_parameters'    => [
         '--class' => 'DatabaseSeeder', // root seeder class
                                        // '--force' => true, // This needs to be true to seed tenant databases in production
     ],
