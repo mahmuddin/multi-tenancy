e0b090d2ee4a0954fbebb0286f8e48035f5e0633
Mahmuddin Nurul Fajri <mahmuddinnf@gmail.com>
2025-06-19 09:36:31 +0700

    Add columns for name, slug, and database to tenants migration

--- database/migrations/2025_06_19_023519_create_tenants_table.php
+++ database/migrations/2025_06_19_023519_create_tenants_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tenants', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique(); // untuk path-based
+            $table->string('database');
             $table->timestamps();
         });
     }
